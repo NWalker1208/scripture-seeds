3d8adac81b6efde9b2b46ceb49b498f7b8a8c24e
Nathan Walker <nathanwalker1208@gmail.com>
2020-05-09 16:08:05 -0700

    Added Sun

--- Assets/Sun2D.cs
+++ Assets/Sun2D.cs
@@ -1,18 +1,38 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
 
 public class Sun2D : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float distance;
+    public float maxIntensity;
+    public float timeOffset;
+
+    Light2D sunlight;
+    Camera mainCamera;
+
+    private void Start()
     {
-        
+        sunlight = GetComponent<Light2D>();
+        mainCamera = Camera.main;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        System.TimeSpan time = System.DateTime.Now.TimeOfDay;
+        double sunAngle = ((time.TotalMinutes + timeOffset) / (24 * 60)) * 2 * Mathf.PI;
+        sunAngle = sunAngle % (2 * Mathf.PI);
+
+        Vector3 pos = transform.position;
+        pos.x = distance * -Mathf.Sin((float)sunAngle);
+        pos.y = distance * -Mathf.Cos((float)sunAngle);
+        transform.position = pos;
+
+        float intensity = Mathf.Cos(((float)sunAngle - Mathf.PI) * 0.75f) * maxIntensity;
+        sunlight.intensity = intensity > 0 ? intensity : 0;
     }
 }
