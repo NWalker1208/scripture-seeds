6214cd4ce01efbdec58b319796832de412586f06
Nathan Walker <nathanwalker1208@gmail.com>
2020-05-20 21:10:44 -0700

    Implemented rudimentary swipe-gesture highlighting

--- Flutter/seeds/lib/services/highlight_text.dart
+++ Flutter/seeds/lib/services/highlight_text.dart
@@ -1,5 +1,6 @@
-import 'dart:math';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 class HighlightText extends StatefulWidget {
   final String text;
@@ -15,7 +16,24 @@ class HighlightText extends StatefulWidget {
 }
 
 class _HighlightTextState extends State<HighlightText> {
+  List<GlobalKey> wordKeys;
   List<bool> highlight;
+
+  int getWordAt(Offset globalPos)
+  {
+    for (int i = 0; i < wordKeys.length; i++)
+    {
+      RenderBox wordBox = wordKeys[i].currentContext.findRenderObject();
+      Size size = wordBox.size;
+      Offset localPos = wordBox.globalToLocal(globalPos);
+
+      if (localPos.dx > 0 && localPos.dx < size.width &&
+          localPos.dy > 0 && localPos.dy < size.height)
+        return i;
+    }
+
+    return null;
+  }
 
   List<Widget> buildParagraph(BuildContext context)
   {
@@ -23,28 +41,28 @@ class _HighlightTextState extends State<HighlightText> {
 
     for (int i = 0; i < widget.words.length; i++) {
       // Padding to separate words
-      children[i] = AnimatedContainer(
-        duration: Duration(milliseconds: 100),
-
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.horizontal(
-            left: (i == 0 || !highlight[i - 1]) ? Radius.circular(5) : Radius.zero,
-            right: (i == (widget.words.length - 1) || !highlight[i + 1]) ? Radius.circular(5) : Radius.zero
+      
+      children[i] = GestureDetector(
+        onTap: () {
+          setState(() {
+            highlight[i] = !highlight[i];
+          });
+        },
+        
+        child: AnimatedContainer(
+          key: wordKeys[i],
+          duration: Duration(milliseconds: 100),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.horizontal(
+              left: (i == 0 || !highlight[i - 1]) ? Radius.circular(5) : Radius.zero,
+              right: (i == (widget.words.length - 1) || !highlight[i + 1]) ? Radius.circular(5) : Radius.zero
+            ),
+            
+            color: Theme.of(context).textSelectionColor.withAlpha(highlight[i] ? 255 : 0),
           ),
           
-          color: Theme.of(context).textSelectionColor.withAlpha(highlight[i] ? 255 : 0),
-        ),
-
-        child: Padding(
-          padding: EdgeInsets.symmetric(horizontal: 2.5, vertical: 2),
-
-          child: GestureDetector(
-            onTap: () {
-              setState(() {
-                highlight[i] = !highlight[i];
-              });
-            },
-
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 2.5, vertical: 2),
             child: Text(
               '${widget.words[i]}',
               style: Theme.of(context).textTheme.bodyText2.merge(TextStyle(
@@ -60,64 +78,66 @@ class _HighlightTextState extends State<HighlightText> {
     return children;
   }
 
-  Offset highlightStart;
-  Offset highlightEnd;
-
   @override
   void initState() {
     super.initState();
     highlight = List<bool>.filled(widget.words.length, false);
+    wordKeys = highlight.map((e) => GlobalKey()).toList();
   }
+
+  int highlightStart;
+  int highlightEnd;
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
       // Detect highlighting gestures
-      /*onLongPressStart: (LongPressStartDetails details) {
+      onLongPressStart: (LongPressStartDetails details) {
+        highlightStart = getWordAt(details.globalPosition);
+        highlightEnd = highlightStart;
+
         setState(() {
-          highlightStart = details.localPosition;
-          highlightEnd = details.localPosition;
+          highlight[highlightStart] = true;
         });
       },
       onLongPressMoveUpdate: (LongPressMoveUpdateDetails details) {
-        setState(() {
-          highlightEnd = details.localPosition;
-        });
+        int currentWord = getWordAt(details.globalPosition);
+        if (currentWord != null && currentWord != highlightEnd) {
+          highlightEnd = currentWord;
+
+          setState(() {
+            if (highlightStart <= highlightEnd)
+              for (int i = highlightStart; i <= highlightEnd; i++)
+                highlight[i] = true;
+            else
+              for (int i = highlightStart; i >= highlightEnd; i--)
+                highlight[i] = true;
+          });
+        }
       },
       onLongPressEnd: (LongPressEndDetails details) {
+        int currentWord = getWordAt(details.globalPosition);
+        if (currentWord != null)
+          highlightEnd = currentWord;
+
+        print('$highlightStart -> $highlightEnd');
+
         setState(() {
-          highlightEnd = details.localPosition;
+          if (highlightStart <= highlightEnd)
+            for (int i = highlightStart; i <= highlightEnd; i++)
+              highlight[i] = true;
+          else
+            for (int i = highlightStart; i >= highlightEnd; i--)
+              highlight[i] = true;
         });
-      },*/
+      },
 
-      child: Stack(
-        overflow: Overflow.visible,
-        alignment: Alignment.center,
-
-        children: <Widget>[
-          // Highlight visual
-          /*Positioned(
-            left: highlightStart == null ? 0 : min(highlightStart.dx, highlightEnd.dx),
-            top: highlightStart == null ? 0 : min(highlightStart.dy, highlightEnd.dy),
-            width: highlightEnd == null ? 0 : (highlightEnd - highlightStart).dx.abs(),
-            height: highlightEnd == null ? 0 : (highlightEnd - highlightStart).dy.abs(),
-            child: Container(
-              decoration: BoxDecoration(
-                color: Theme.of(context).textSelectionColor,
-                borderRadius: BorderRadius.circular(5)
-              ),
-            )
-          ),*/
-
-          // Text
-          Wrap(
-            direction: Axis.horizontal,
-            alignment: WrapAlignment.start,
-            spacing: -1,
-            runSpacing: 4,
-            children: buildParagraph(context)
-          )
-        ]
+      child: Wrap(
+        direction: Axis.horizontal,
+        alignment: WrapAlignment.start,
+        spacing: -1,
+        runSpacing: 4,
+        children: buildParagraph(context)
       ),
     );
   }
