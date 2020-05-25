447eb0b7bf3de50f18ee83d3cba0f504a442d694
Nathan Walker <nathanwalker1208@gmail.com>
2020-05-25 15:26:48 -0700

    Added more scriptures

--- Flutter/seeds/lib/services/library.dart
+++ Flutter/seeds/lib/services/library.dart
@@ -4,22 +4,48 @@ import 'package:seeds/services/scripture.dart';
 class Library {
 
   static final Map<String, Map<int, Map<int, String>>> scriptures = {
+    /// Old Testament
+    'Job': {
+      2: {
+        3: 'And the Lord said unto Satan, Hast thou considered my servant Job, ' +
+           'that there is none like him in the earth, a perfect and an upright ' +
+           'man, one that feareth God, and escheweth evil? and still he holdeth ' +
+           'fast his integrity, although thou movedst me against him, to destroy ' +
+           'him without cause.',
+      },
+    },
+
+    /// New Testament
     'Matthew': {
       17: {
-        20: 'And Jesus said unto them, Because of your unbelief: for verily I say unto you, If ye have faith as a grain of mustard seed, ye shall say unto this mountain, Remove hence to yonder place; and it shall remove; and nothing shall be impossible unto you.'
-      }
-    }
+        20: 'And Jesus said unto them, Because of your unbelief: for verily ' +
+            'I say unto you, If ye have faith as a grain of mustard seed, ye ' +
+            'shall say unto this mountain, Remove hence to yonder place; and ' +
+            'it shall remove; and nothing shall be impossible unto you.',
+      },
+    },
+
+    /// Book of Mormon
+    'Moroni': {
+      9: {
+        6: 'And now, my beloved son, notwithstanding their hardness, let us ' +
+           'labor diligently; for if we should cease to labor, we should be ' +
+           'brought under condemnation; for we have a labor to perform whilst ' +
+           'in this tabernacle of clay, that we may conquer the enemy of all ' +
+           'righteousness, and rest our souls in the kingdom of God.',
+      },
+    },
   };
 
   static final SplayTreeMap<String, List<List<Scripture>>> topics = SplayTreeMap.from({
     'diligence': [
-      [Scripture('Matthew',17,20)]
+      [Scripture('Moroni',9,6)]
     ],
     'faith': [
       [Scripture('Matthew',17,20)]
     ],
     'integrity': [
-      [Scripture('Matthew',17,20)]
+      [Scripture('Job',2,3)]
     ],
   });
 
